810000c9040d77f7d5b11134a6c59941f64cbb58
srijakommalapati <21211a7228@bvrit.ac.in>
2024-12-23 15:46:10 +0530

    Fix: Bind to PORT environment variable for Render

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify
 import random
 import time
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 
@@ -122,4 +123,5 @@ def get_traffic_signal():
     return jsonify(signal_decision)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Use the PORT environment variable or default to 5000
+    app.run(host='0.0.0.0', port=port, debug=True)
